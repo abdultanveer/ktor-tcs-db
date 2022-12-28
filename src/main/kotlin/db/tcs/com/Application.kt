feec9bc0d11cafd63b6fe0e6713f45db7c08360c
Abdul <abdul.tanveer@gmail.com>
2022-12-28 10:08:38 +0530

    initial commit

---
+++ src/main/kotlin/db/tcs/com/Application.kt
@@ -0,0 +1,12 @@
+package db.tcs.com
+
+import io.ktor.server.application.*
+import db.tcs.com.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
